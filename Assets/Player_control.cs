054d84b296cd936656405ba1d6bb7334ce208668
Thirdsense3 <31949361+Thirdsense3@users.noreply.github.com>
2017-11-07 16:41:59 +0900

    캡슐과 큐브로 인간구현, 카메라 인간시점으로 바꿈, control 스크립트 생성

---
+++ Assets/Player_control.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_control : MonoBehaviour {
+    private var hasAxe : boolean = false;
+    private var canSwing : boolean = true;
+  

67e6d824d36c2c56936d9891d1f12201eaed3808
DONGHYUN <velvet64@naver.com>
2017-11-11 01:11:40 +0900

    * 낚시대 드디어 만들어서 추가 * 동현이의 연습용 scene 'dong' 생성 * 쓸모없는 2d 샘플들 삭제

--- Assets/Player_control.cs
+++ Assets/Player_control.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_control : MonoBehaviour {
+    /*
     private var hasAxe : boolean = false;
     private var canSwing : boolean = true;
-  
+        */
+  }
---
+++ Assets/Script/Swing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) transform.Rotate(0, 0, 45 * Time.deltaTime);
+    }
+}

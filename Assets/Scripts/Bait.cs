89c29ddf3e746bf27b8b5dc8c5e0a687198f4a72
DONGHYUN <velvet64@naver.com>
2017-12-14 14:26:34 +0900

    * 코드에 주석 달기

--- Assets/Scripts/Bait.cs
+++ Assets/Scripts/Bait.cs
@@ -35,25 +35,25 @@ public class Bait : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(BaitActive)
+		if(BaitActive) //미끼가 활성화 됐을 때
 		{
 			if(Time.time > m_StepTemp + m_currentUpdateStep) 
 			{
-				for(int i = 0; i<TargetFishes.Count; i++)
+				for(int i = 0; i<TargetFishes.Count; i++) // 물고기의 수만큼 실행 (여기서는 한마리)
 				{
-					float rnd = Random.Range(TargetFishes[i].minCatchChance, TargetFishes[i].maxCatchChance);
-					if(rnd > TargetFishes[i].targetCatchChance)
+					float rnd = Random.Range(TargetFishes[i].minCatchChance, TargetFishes[i].maxCatchChance); // rnd 값이 계속 min ~ max 사이에 랜덤으로 정해짐
+					if(rnd > TargetFishes[i].targetCatchChance) // 정해진 CatchChance 값보다 rnd가 커지면 물고기가 생성(미끼를 뭄)
 					{
-						GameObject m_fish = Instantiate(TargetFishes[i].fish.gameObject, transform.position, transform.rotation) as GameObject;
+						GameObject m_fish = Instantiate(TargetFishes[i].fish.gameObject, transform.position, transform.rotation) as GameObject; // instantiate 이용하여 물고기 생성
 				
-						Player.Instance.UpdateStage(PlayStage.Catching);
+						Player.Instance.UpdateStage(PlayStage.Catching); // 플레이어 상태를 Catching 상태로
 						Player.Instance.Spinning.CatchedFish = m_fish.GetComponent<Fish>();
 						transform.SetParent(m_fish.transform);
-						BaitActive = false;
+						BaitActive = false; // 미끼 비활성화
 					}
 				}
 
-				m_currentUpdateStep = Random.Range(minUpdateStep, maxUpdateStep);
+				m_currentUpdateStep = Random.Range(minUpdateStep, maxUpdateStep); // min 값과 max 값 사이에 랜덤하게 계속 값 정함
 				m_StepTemp = Time.time;
 			}
 		}
@@ -63,12 +63,12 @@ public class Bait : MonoBehaviour
 	{
 		if(cast)
 		{
-			if(transform.position.y > minDeep)
+			if(transform.position.y > minDeep) // 최소 깊이 이상으로 물에 잠김
 				transform.Translate(Vector3.down * divingSpeedDown * Time.deltaTime);
 		}
 		else
 		{
-			if(transform.position.y < maxDeep)
+			if(transform.position.y < maxDeep) // 최대 깊이 이하로 물에 잠김
 				transform.Translate(Vector3.up * divingSpeedUp * Time.deltaTime);
 		}
 	}
